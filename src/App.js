517c717442a21e4f724719b90dddc630d6c6e78b
riyaz-b <riyazbeg@gmail.com>
2025-05-12 02:12:59 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Login from './components/Auth/Login';
 import Dashboard from './components/Dashboard/Dashboard';
 import AxiosMenu from './components/AxiosMenu/AxiosMenu';
 import EmployeeList from './components/EmployeeList/EmployeeList';
+import ReduxMenu from './components/ReduxMenu/ReduxMenu';
+import ReactQueryMenu from './components/ReactQueryMenu/ReactQueryMenu';
+import UseEffectDemo from './components/UseEffectDemo/UseEffectDemo';
+import CustomHookDemo from './components/CustomHookDemo/CustomHookDemo';
 import ProtectedRoute from './components/ProtectedRoute'; // Correct path if it's directly in components
 import PaginationMenu from './components/PaginationMenu/PaginationMenu';
 
@@ -43,6 +47,38 @@ const App = () => {
               element={
                 <ProtectedRoute allowedRoles={['admin']}>
                   <PaginationMenu />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="redux-menu"
+              element={
+                <ProtectedRoute allowedRoles={['admin']}>
+                  <ReduxMenu />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="react-query-menu"
+              element={
+                <ProtectedRoute allowedRoles={['admin']}>
+                  <ReactQueryMenu />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="use-effect"
+              element={
+                <ProtectedRoute allowedRoles={['admin']}>
+                  <UseEffectDemo />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="custom-hook"
+              element={
+                <ProtectedRoute allowedRoles={['admin']}>
+                  <CustomHookDemo />
                 </ProtectedRoute>
               }
             />
